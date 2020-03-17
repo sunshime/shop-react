c84d1714bb673aa4c3f791948f3a574bcba501ef
lfj <liao_fengji@163.com>
2020-03-17 10:39:11 +0800

    实现我的订单部分

--- src/pages/my.jsx
+++ src/pages/my.jsx
@@ -4,7 +4,26 @@ import AHeader from "../components/header";
 import Footer from "../components/footer";
 
 class Category extends Component {
-  state = {};
+  state = {
+    orderList: [
+      {
+        imgSrc: "substitutePay.jpg",
+        name: "代付款"
+      },
+      {
+        imgSrc: "collectGoods.jpg",
+        name: "待收货"
+      },
+      {
+        imgSrc: "generationEvaluation.jpg",
+        name: "待评价"
+      },
+      {
+        imgSrc: "refund.jpg",
+        name: "退款/售后"
+      }
+    ]
+  };
   render() {
     return (
       <div className="my">
@@ -18,8 +37,33 @@ class Category extends Component {
               />
               <div className="text">
                 <span className="user-name">molly</span>
-                <span className="sign">时间，带不走真正的朋友；岁月，留不住虚幻的拥有。</span>
+                <span className="sign">
+                  时间，带不走真正的朋友；岁月，留不住虚幻的拥有。
+                </span>
               </div>
+            </div>
+          </div>
+          <div className="order">
+            <div className="order-top">
+              <div className="left">我的订单</div>
+              <div className="right">
+                查看全部订单&nbsp;<span>&gt;</span>
+              </div>
+            </div>
+            <div className="order-bt">
+              <ul className="order-ul">
+                {this.state.orderList.map((item, index) => {
+                  return (
+                    <li key={item+index} className="order-li">
+                      <img
+                        src={require("../common/images/" + item.imgSrc)}
+                        alt=""
+                      />
+                      <span>{item.name}</span>
+                    </li>
+                  );
+                })}
+              </ul>
             </div>
           </div>
         </div>
